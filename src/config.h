40235905e0ca25df153e71843034bff2e4476233
Michael Wuertinger <michael@wuertinger.de>
2020-03-26 22:18:19 +0100

    Replace mqttClient with devId in Config

--- src/config.h
+++ src/config.h
@@ -3,11 +3,11 @@
 
 class Config {
 public:
+    const int devId = 0;
     const char *ssid = "";
     const char *password = "";
     const uint8_t mqttServerIP[4] = {0, 0, 0, 0};
     const int mqttServerPort = 1883;
-    const char *mqttClient = "";
     const char *mqttUser = "";
     const char *mqttPassword = "";
     const char *mqttServerPubKey = "";
