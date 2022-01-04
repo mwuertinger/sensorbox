0c8a51c2b3e4d25711b7f2ec2596a4b65fced71a
Michael Wuertinger <michael@wuertinger.de>
2022-01-04 22:02:57 +0100

    Add checksum verification.

--- config-tool/dump/main.go
+++ config-tool/dump/main.go
@@ -3,21 +3,29 @@ package main
 import (
 	"encoding/binary"
 	"google.golang.org/protobuf/proto"
+	"hash/crc32"
 	"io/ioutil"
 	"log"
 	"os"
 
-	"github.com/mwuertinger/sensorbox/config/pb"
+	"github.com/mwuertinger/sensorbox/config-tool/pb"
 )
 
 func main() {
-	in, err := ioutil.ReadFile(os.Args[2])
+	in, err := ioutil.ReadFile(os.Args[1])
 	if err != nil {
 		log.Fatalln("Error reading file:", err)
 	}
 	length := binary.LittleEndian.Uint32(in[0:4])
+
+	checksum := binary.LittleEndian.Uint32(in[4:8])
+	calculatedChecksum := crc32.ChecksumIEEE(in[8 : length+8])
+	if checksum != calculatedChecksum {
+		log.Fatalf("Checksum mismatch: provided=%d, calculated=%d", checksum, calculatedChecksum)
+	}
+
 	config := &pb.ConfigPb{}
-	if err := proto.Unmarshal(in[4:length+4], config); err != nil {
+	if err := proto.Unmarshal(in[8:length+8], config); err != nil {
 		log.Fatalln("Failed to parse address book:", err)
 	}
 
