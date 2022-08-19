e0cc7711dfe84d23c93c9ce48b54a3b04d3a75d3
Michael Wuertinger <michael@wuertinger.de>
2022-08-19 09:00:39 +0200

    Use central protobuf compilation script.

--- firmware/protoc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -ex
-pwd
-nanopb_generator.py -D src -I .. ../sensorbox.proto
--- protoc.sh
+++ protoc.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
 set -e
 mkdir -p server/pb
 mkdir -p config-tool/pb
 protoc -I. -I/usr/lib/python3/dist-packages/proto --go_out=paths=source_relative:server/pb --go_opt=Mnanopb.proto=github.com/mwuertinger/sensorbox/server/pb sensorbox.proto
 protoc -I. -I/usr/lib/python3/dist-packages/proto --go_out=paths=source_relative:config-tool/pb --go_opt=Mnanopb.proto=github.com/mwuertinger/sensorbox/config-tool/pb sensorbox.proto
+nanopb_generator.py -D firmware/src -I . sensorbox.proto
