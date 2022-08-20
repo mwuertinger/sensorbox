9cf7d9c1058609434f46a85b6b8ee850c3f5709f
Michael Wuertinger <michael@wuertinger.de>
2022-08-20 10:05:58 +0200

    Add config, auth and InfluxDB support.

--- server/go.mod
+++ server/go.mod
@@ -3,5 +3,7 @@ module github.com/mwuertinger/sensorbox/server
 go 1.18
 
 require (
+	github.com/influxdata/influxdb1-client v0.0.0-20220302092344-a9ab5670611c
 	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0
 )
--- server/main.go
+++ server/main.go
@@ -1,22 +1,47 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
-	"google.golang.org/protobuf/proto"
-
+	influxdb "github.com/influxdata/influxdb1-client/v2"
 	"github.com/mwuertinger/sensorbox/server/pb"
+	"google.golang.org/protobuf/proto"
+	"gopkg.in/yaml.v2"
 )
 
+var config *Config
+var influxClient influxdb.Client
+
 func main() {
-	certFile := ""
-	keyFile := ""
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc, os.Interrupt, syscall.SIGTERM)
+
+	if len(os.Args) != 2 {
+		log.Fatalf("Usage: %s <config>", os.Args[0])
+	}
+	var err error
+	config, err = parseConfig(os.Args[1])
+	if err != nil {
+		log.Fatalf("loading config failed: %v", err)
+	}
+
+	influxClient, err = influxdb.NewHTTPClient(influxdb.HTTPConfig{
+		Addr: config.Influx.Server,
+	})
+	if err != nil {
+		log.Fatalf("influx initialization: %v", err.Error())
+	}
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/sensorbox", handler)
+	mux.HandleFunc("/sensorbox", httpHandler)
 
 	s := &http.Server{
 		Addr:           ":8080",
@@ -25,10 +50,25 @@ func main() {
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
-	log.Fatal(s.ListenAndServeTLS(certFile, keyFile))
+	go func() {
+		if err := s.ListenAndServeTLS(config.CertFile, config.KeyFile); err != http.ErrServerClosed {
+			log.Fatalf("http: %v", err)
+		}
+	}()
+
+	sig := <-sigc
+	log.Printf("received %v -> shutting down...", sig)
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
+	if err := s.Shutdown(ctx); err != nil {
+		log.Printf("http: %v", err)
+	}
+	if err := influxClient.Close(); err != nil {
+		log.Printf("influx: %v", err)
+	}
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func httpHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	buf, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -43,10 +83,18 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// process request
+	response, err := handleRequest(&request)
+	if err == unauthorizedErr {
+		w.WriteHeader(401)
+		return
+	}
+	if err != nil {
+		log.Printf("handleRequest: %v", err)
+		w.WriteHeader(500)
+		return
+	}
 
-	response := pb.Response{}
-	data, err := proto.Marshal(&response)
+	data, err := proto.Marshal(response)
 	if err != nil {
 		log.Printf("marshal: %v", err)
 		w.WriteHeader(500)
@@ -59,4 +107,100 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(500)
 		return
 	}
+}
+
+var unauthorizedErr = errors.New("unauthorized")
+
+func handleRequest(request *pb.Request) (*pb.Response, error) {
+	dev, ok := config.Devices[int(request.DevId)]
+	if !ok {
+		log.Printf("unknown device: %d", request.DevId)
+		return nil, unauthorizedErr
+	}
+	if dev.Passwd != request.Passwd {
+		log.Printf("wrong passwd for device: %d", request.DevId)
+		return nil, unauthorizedErr
+	}
+
+	err := writeToInflux(dev.Location, request.Measurement)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.Response{}, nil
+}
+
+// writeToInflux writes measurements to InfluxDB
+func writeToInflux(location string, m *pb.Measurement) error {
+	// Create a new point batch
+	bp, err := influxdb.NewBatchPoints(influxdb.BatchPointsConfig{
+		Database:  "sensors",
+		Precision: "s",
+	})
+	if err != nil {
+		return err
+	}
+	tags := map[string]string{"location": location}
+	fields := map[string]interface{}{}
+	if m.Pressure > 0 {
+		fields["pressure"] = m.Pressure
+	}
+	if m.Humidity > 0 {
+		fields["humidity"] = m.Humidity
+	}
+	if m.Temperature > 0 {
+		fields["temperature"] = m.Temperature - 273.15 // convert to °C
+	}
+	if m.Co2 > 0 {
+		fields["co2"] = m.Co2
+	}
+	if m.SoilMoisture > 0 {
+		fields["soil_moisture"] = m.SoilMoisture
+	}
+
+	log.Printf("writing to influx: %+v", fields)
+
+	pt, err := influxdb.NewPoint("measurements", tags, fields, time.Now())
+	if err != nil {
+		return err
+	}
+	bp.AddPoint(pt)
+
+	// Write the batch
+	if err := influxClient.Write(bp); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Config represents a config file
+type Config struct {
+	Influx   InfluxConfig   `yaml:"influx"`
+	Devices  map[int]Device `yaml:"devices"`
+	CertFile string
+	KeyFile  string
+}
+
+type InfluxConfig struct {
+	Server string `yaml:"server"`
+	Token  string `yaml:"token"`
+}
+
+type Device struct {
+	Location string `yaml:"location"`
+	Passwd   string `yaml:"passwd"`
+}
+
+// parseConfig reads config file at path and returns the content or an error
+func parseConfig(path string) (*Config, error) {
+	buf, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var config Config
+	err = yaml.Unmarshal(buf, &config)
+	if err != nil {
+		return nil, err
+	}
+	return &config, nil
 }
